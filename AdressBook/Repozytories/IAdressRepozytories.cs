aa8a89628a7071aa861b68889ccab62e148eac9d
Mateusz <49116164+Ricoss@users.noreply.github.com>
2023-09-08 09:32:53 +0200

    Add Repozytories

---
+++ AdressBook/Repozytories/IAdressRepozytories.cs
@@ -0,0 +1,16 @@
+﻿using AdressBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdressBook.Repozytories
+{
+    public interface IAdressRepozytories
+    {
+        Task AddAssync(Adress adress);
+        Task<Adress> GetCityAssync(string city);
+        Task<IEnumerable<Adress>> GetAllAssync();
+        Task UpdataAssync (Adress adress);
+        Task DeleteAssync(Guid id);
+    }
+}
